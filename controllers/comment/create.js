c067f103e0597f8d91de6e583615cb0973cdc881
Thomas Bredillet <tnbredillet@gmail.com>
2014-06-18 12:53:37 +0200

    travis ci fix

--- controllers/comment/create.js
+++ controllers/comment/create.js
@@ -1,17 +1,17 @@
 var models = require(__dirname + "/../../models");
 
 module.exports = function(req, res) {
-  var commentData = req.param("comment",null);
+  var commentData = req.param("comment", null);
   var metaData = req.metaData;
   var Comment = models.Comment;
 
   var comment = new Comment({
-        comment : commentData.text,
-        user_id  : metaData.current_user.id,
-        list_id  : commentData.listId
-      });
+    comment: commentData.text,
+    user_id: metaData.current_user.id,
+    list_id: commentData.listId
+  });
   comment.save()
 
-  res.redirect("/lists/"+commentData.listId);
- 
+  res.redirect("/lists/" + commentData.listId);
+
 };
