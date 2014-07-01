af8c860b10a528e2df1a9aceb1c6aa5119dee1b0
Antoine Dematteo <antoinedematteo@gmail.com>
2014-07-01 17:04:47 +0200

    validations/flash

--- controllers/login.js
+++ controllers/login.js
@@ -1,9 +1,19 @@
 var models = require(__dirname + "/../models");
 
 module.exports = function(req, res) {
-      res.render(
-        "static/login", {
-          metaData: req.metaData
-        }
-      );
-    }
+	message = [{
+		alertType: "alert-danger",
+		strongMessage: "Error!",
+		messageText: req.flash( 'error' )[0],
+		display: true
+	}];
+	if( !message[0].messageText ){ message = null;}
+	console.log( message );
+	res.render(
+		"static/login", {
+			metaData: req.metaData,
+			message: message
+		}
+	)
+}
+
