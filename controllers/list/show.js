eaafccd7c1a3f8f3c404288fc61c32057fbf64da
adematteo <antoinedematteo@gmail.com>
2014-07-26 11:01:03 +0200

    notification comment+Reply

--- controllers/list/show.js
+++ controllers/list/show.js
@@ -16,6 +16,7 @@ module.exports = function(req, res) {
 		commentsSorted = list.related("comments").toJSON().sort(function(a, b) {
 			return b.created_at.getTime() - a.created_at.getTime()
 		});
+		message = null;
 		for (var i = 0; i < commentsSorted.length; i++) {
 			var disabled=""
 			for (var j = 0; j < commentsSorted[i].likes.length; j++) {
@@ -32,12 +33,52 @@ module.exports = function(req, res) {
 			else{
 				commentsSorted[i].textDisplayReply = 'Click to see the reply';
 			};
+			if( message == null ){
+				for( k =0 ; k < commentsSorted[i].replies.length ;k++){
+					if( Math.floor( (new Date() - commentsSorted[i].replies[k].created_at.getTime() ) / 1000) < 10){
+						if( commentsSorted[i].replies[k].user.nickname == metaData.current_user.nickname ){
+							message = [{
+								alertType: "alert-success",
+								strongMessage: "Reply saved!",
+								messageText: "",
+								display: true
+							}]				
+
+						}
+					}				
+				}
+			}
+
 
 		};
+
+		//code to check if the user just sent a new message
+		if( message == null ){
+			if( Math.floor( (new Date() - commentsSorted[0].created_at.getTime() ) / 1000) < 10){
+				if( commentsSorted[0].user.nickname == metaData.current_user.nickname ){
+					message = [{
+						alertType: "alert-success",
+						strongMessage: "Message saved!",
+						messageText: "",
+						display: true
+					}]				
+				}
+				else{
+					message = null;
+				}
+
+			}
+			else{
+				message = null;
+			}			
+		}
+
+
 		res.render("list/show", {
 			metaData: req.metaData,
 			list: list.toJSON(),
 			comments: commentsSorted,
+			message: message,
 		});
 	})
 };
