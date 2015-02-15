9eb251f4a7a11feb71da56c75c598e99b9987dba
AntoineDematteo <antoinedematteo@gmail.com>
2015-02-15 20:41:31 +0100

    les admins voient toutes les listes

--- controllers/user/home.js
+++ controllers/user/home.js
@@ -1,4 +1,6 @@
 var models = require(__dirname + "/../../models");
+var Bookshelf = require('bookshelf').PG;
+
 module.exports = function(req, res) {
 
   if( req.flash( 'success' )[0] == 'success' ){
@@ -23,8 +25,15 @@ module.exports = function(req, res) {
     update_time = current_user.get( 'updated_at' );
     current_user.save( { lastlogin_at : update_time , logincount: logCount });
   });
-  user.lists().fetch().then(function(lists){
+  console.log( current_user.admin )
+  if( current_user.admin ){
+    var List = models.List;
+    var list = new List;
 
+  var Lists = Bookshelf.Collection.extend({
+    model: List
+  });
+  var lists = new Lists().query('where', 'id', '>', 0).fetch().then(function(lists) {
     res.render("user/home", {
       metaData: req.metaData,
       lists: lists.toJSON(),
@@ -32,5 +41,15 @@ module.exports = function(req, res) {
     });
   });
 
+ }
+  else{
+    user.lists().fetch().then(function(lists){
 
-};
+      res.render("user/home", {
+        metaData: req.metaData,
+        lists: lists.toJSON(),
+        message: messageLoginSuccess
+      });
+    });
+  };
+}
